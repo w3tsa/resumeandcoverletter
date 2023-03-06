68c1a02089e262cdcc24ca9314e791a937e41756
Tanveer Sayem <tanveersayem@gmail.com>
2023-03-06 15:26:42 -0500

    Removed the start github as it is no longer public

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Heading, Link, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 // styles
 import styles from "@/styles/Hero.module.css";
-import { BsGithub } from "react-icons/bs";
+// import { BsGithub } from "react-icons/bs";
 
 type Props = {
   count: number;
@@ -12,7 +12,7 @@ const Hero = (props: Props) => {
   const { count } = props;
   return (
     <Box mt={5}>
-      <Link href="https://github.com/w3tsa/writemecoverletter" isExternal>
+      {/* <Link href="https://github.com/w3tsa/writemecoverletter" isExternal>
         <Button
           leftIcon={<BsGithub />}
           colorScheme="gray"
@@ -21,7 +21,7 @@ const Hero = (props: Props) => {
         >
           Star on GitHub
         </Button>
-      </Link>
+      </Link> */}
       <Heading as="h1" size="3xl" mt={5}>
         Generate your next{" "}
         <span className={styles.boujee}>Resume/cover-letter</span> using chatGPT
@@ -35,7 +35,3 @@ const Hero = (props: Props) => {
 };
 
 export default Hero;
-
-/* 
-<span className="boujee-text">Resume.</span>
-*/
