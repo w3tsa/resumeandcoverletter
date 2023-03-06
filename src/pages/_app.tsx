255ddf62c93149643007278bb3553f6cefa5cbee
Tanveer Sayem <tanveersayem@gmail.com>
2023-03-06 15:25:24 -0500

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
+
 import theme from "../../theme";
 
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+    </>
   );
 }
